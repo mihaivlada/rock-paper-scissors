ef0a2b6a87ff79735acdb98ebade990cf2141a9e
drugh <darklightme73@gmail.com>
2020-10-29 12:04:31 +0200

    added a graphical interface to the game

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+// Improved rock paper scissors game with UI
 
-// write a function computerPlay() that will return a random between 3 numbers
-// make the function return a sting associated with each number
+
+const container = document.querySelector('#buttons');
+
+let outputText = document.createElement('h4');
+// add a class before styling
+
+let outputScore = document.createElement('p');
+// add a class before styling
+
+const allButtons = document.querySelectorAll('button');
+
+allButtons.forEach(button => button.addEventListener('click', function(e) {
+    outcome(e.target.textContent.toLowerCase(), computerPlay());
+})
+);
+
+
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 1) {
@@ -10,81 +26,49 @@ function computerPlay() {
         return "paper";
     }
     else if (randomNumber === 3) {
-        return "scissors"
+        return "scissors";
     }
 }
 
-// we declare a global variable to store the player score
 let playerScore = 0;
-// we declare a global variable to store the computer score
 let computerScore = 0;
 
-// write a function with 2 parameters that evaluates the playerSelection and the computerSelection
-// and displays a string if you win or lose
 function outcome(playerSelection, computerSelection) {
-    if ((playerSelection === "rock") && (computerSelection === "scissors")) {
-        // we increase the player score each time the player wins
+    if ((playerSelection !== "rock") && (playerSelection !== "paper") && (playerSelection !== "scissors")) {
+        outputText.textContent = "You entered the wrong thing!\nYou need to choose from rock, paper or scissors";
+        return;
+    }
+    else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
         playerScore++;
-        return `You won! ${playerSelection} beats ${computerSelection}`;
+        outputText.textContent = `You won! ${playerSelection} beats ${computerSelection}`;
+        outputScore.textContent = `Player score : ${playerScore}  Computer score : ${computerScore}`;
+        return;
     }
     else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-        // we increase the player score each time the player wins
         playerScore++;
-        return `You won! ${playerSelection} beats ${computerSelection}`;
+        outputText.textContent = `You won! ${playerSelection} beats ${computerSelection}`;
+        outputScore.textContent = `Player score : ${playerScore}  Computer score : ${computerScore}`;
+        return;
     }
     else if ((playerSelection === "paper") && (computerSelection === "rock")) {
-        // we increase the player score each time the player wins
         playerScore++;
-        return `You won! ${playerSelection} beats ${computerSelection}`;
+        outputText.textContent = `You won! ${playerSelection} beats ${computerSelection}`;
+        outputScore.textContent = `Player score : ${playerScore}  Computer score : ${computerScore}`;
+        return;
     }
     else if (computerSelection === playerSelection) {
-        return `It's a tie!`;
-    }
-    else {
-        // we increase the computer score each time the computer wins
-        computerScore++;
-        return `You lost! ${computerSelection} beats ${playerSelection}`;
-    }
-}
-
-// write a function game() that will play one round
-// it takes as parameter the playerSelection and displays an error if the string was wrong
-// if they enter the right string, call the outcome() function and return the outcome
-
-function game(selection) {
-    if ((selection !== "rock") && (selection !== "paper") && (selection !== "scissors")) {
-        console.log("You entered the wrong thing!\nYou need to choose from rock, paper or scissors");
+        outputText.textContent = `It's a tie!`;
         return;
     }
     else {
-        return outcome(selection, computerPlay());
+        computerScore++;
+        outputText.textContent = `You lost! ${computerSelection} beats ${playerSelection}`;
+        outputScore.textContent = `Player score : ${playerScore}  Computer score : ${computerScore}`;
+        return;
     }
 }
 
-// we declare a contor
-let i = 0;
 
-// we do a while loop to play 5 times
-while (i < 5) {
+container.appendChild(outputText);
+container.appendChild(outputScore);
 
-    // get input from the user
-    // put that input in a variable
-    // make that input case insesitive
-    let result = prompt("Choose rock, paper or scissors!");
-    result = result.toLowerCase();
-    // play the game!
-    console.log(game(result));
-    // we display the score after each round
-    console.log(`Player score : ${playerScore}  Computer score : ${computerScore}`);
-    // we increase the contor
-    i++;
-}
-if (playerScore > computerScore) {
-    console.log("YOU ARE THE WINNER! CONGRATS");
-}
-else if(playerScore < computerScore){
-    console.log("Computer wins");
-}
-else {
-    console.log("It's a draw!");
-}
